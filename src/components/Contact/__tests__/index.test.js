bef4995370d83e59c5dba7dd0e969034810a54cf
Julio.S <just.julio95@gmail.com>
2022-07-22 10:29:46 -0400

    added tests for contact form

---
+++ src/components/Contact/__tests__/index.test.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { render, cleanup } from "@testing-library/react";
+import '@testing-library/jest-dom/extend-expect';
+import ContactForm from "..";
+
+afterEach(cleanup)
+
+describe('ContactForm component', () => {
+  it('renders', () => {
+    render(<ContactForm></ContactForm>)
+  })
+
+  it('matches snapshot', () => {
+    const { asFragment } = render(<ContactForm></ContactForm>)
+
+    expect(asFragment()).toMatchSnapshot();
+  })
+
+  it('renders', () => {
+    const { getByTestId } = render(<ContactForm></ContactForm>)
+    expect(getByTestId('contact-me')).toHaveTextContent('Contact me')
+  })
+
+  it('renders', () => {
+    const { getByTestId } = render(<ContactForm></ContactForm>)
+    expect(getByTestId('submit')).toHaveTextContent('Submit')
+  })
+
+})
